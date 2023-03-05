635447c2437fbf68b8faf3b5835a4ff2aa49a7af
Artem Mirvoda <mirvodartem@gmail.com>
2023-03-05 20:09:23 +0500

    Added consts

--- consts.py
+++ consts.py
@@ -1,11 +1,7 @@
-import numpy as np
-
 RANDOM_STATE = 42
 
 DATA_TEST_SIZE = .3
 DATA_N = 10_000
-DATA_NOISE_POWER = .1
-DATA_FUNCTION = lambda x: np.power(x, 2) - 5 * x
 
 Y_TRAIN = '.\\train\\'
 Y_TRAIN_FULL = Y_TRAIN + 'y.pckl'
@@ -17,3 +13,6 @@ X_TEST = '.\\test\\'
 X_TEST_FULL = X_TEST + 'x.pckl'
 
 DATA_PATH = '.\\data_default\\' + 'train.csv'
+
+MODEL = '.\\model\\'
+MODEL_FULL = MODEL + 'model.pckl'

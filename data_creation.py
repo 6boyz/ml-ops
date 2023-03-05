b848799ec0e875e52449bc5506c9d6ca097116a5
Artem Mirvoda <mirvodartem@gmail.com>
2023-03-05 19:49:57 +0500

    Renamed file and const name

--- data_creation.py
+++ data_creation.py
@@ -8,7 +8,7 @@ import consts
 from utils import time_now
 
 
-data = pd.read_csv(consts.TRAIN_PATH, delimiter=',', index_col='Id')
+data = pd.read_csv(consts.DATA_PATH, delimiter=',', index_col='Id')
 x = data.iloc[:, 0:-1]
 y = data.SalePrice.values
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=consts.DATA_TEST_SIZE, random_state=consts.RANDOM_STATE)

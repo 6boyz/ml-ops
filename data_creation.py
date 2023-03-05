0ea22159c0a49cea0578604c7618dae066f2590a
Artem Kasov <kasovarteom@yandex.ru>
2023-03-05 19:42:35 +0500

    begin data preprocessing

--- data_creation.py
+++ data_creation.py
@@ -2,44 +2,19 @@ import pickle
 import numpy as np
 from pathlib import Path
 from sklearn.model_selection import train_test_split
+import pandas as pd
 
 import consts
 from utils import time_now
 
-def true_fun(x, a=np.pi, b = 0, f=np.sin):
-  x = np.atleast_1d(x)[:]
-  a = np.atleast_1d(a)
-  
-  if f is None: f = lambda x:x # line
-  x = np.sum([ai*np.power(x, i+1) for i,ai in enumerate(a)],axis=0)
 
-  return f(x+ b)
-
-def noises(shape , noise_power):
-  return np.random.randn(*shape) * noise_power
-
-def dataset(a, b, f = None,  N = 250, x_max =1, noise_power = 0, random_x = True,  seed = 42):
-  np.random.seed(seed)
-  
-  if random_x:
-    x = np.sort(np.random.rand(N))*x_max    
-  else:
-    x = np.linspace(0,x_max,N)
-  
-  y_true = np.array([])
-  
-  for f_ in np.append([], f):
-    y_true=np.append(y_true, true_fun(x, a, b, f_))
-  
-  y_true = y_true.reshape(-1,N).T
-  y = y_true + noises(y_true.shape , noise_power)
-
-  return y, y_true, np.atleast_2d(x).T
-
+data = pd.read_csv(consts.TRAIN_PATH, delimiter=',', index_col='Id')
+x = data.iloc[:, 0:-1]
+y = data.SalePrice.values
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=consts.DATA_TEST_SIZE, random_state=consts.RANDOM_STATE)
 
 if __name__ == '__main__':
-  print(f'{time_now()} Генерация данных')
-  y, _, x = dataset(a = [1,4,-2], b = 1, f = consts.DATA_FUNCTION, N = consts.DATA_N, x_max = 1, noise_power = consts.DATA_NOISE_POWER, seed = 42)
+  print(f'{time_now()} Загрузка данных')
   X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=consts.DATA_TEST_SIZE, random_state=consts.RANDOM_STATE)
   
   print(f'{time_now()} Сохранение данных')
---
+++ model_preprocessing.py
@@ -0,0 +1,15 @@
+import pickle
+import pandas as pd
+import consts
+import re
+
+x_train = pd.read_pickle(consts.X_TRAIN_FULL)
+y_train = pd.read_pickle(consts.Y_TRAIN_FULL)
+
+x_test = pd.read_pickle(consts.X_TEST_FULL)
+y_test = pd.read_pickle(consts.Y_TEST_FULL)
+
+print(y_train)
+
+
+#

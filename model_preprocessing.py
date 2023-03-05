62aa66f8854798b98c34a97303a3558fcbdc670d
Artem Kasov <kasovarteom@yandex.ru>
2023-03-05 20:39:47 +0500

    creating model_preprocessing.py

--- model_preprocessing.py
+++ model_preprocessing.py
@@ -1,7 +1,9 @@
 import pickle
 import pandas as pd
+import numpy as np
 import consts
-import re
+from scipy.stats import norm, skew
+from sklearn.preprocessing import LabelEncoder
 
 x_train = pd.read_pickle(consts.X_TRAIN_FULL)
 y_train = pd.read_pickle(consts.Y_TRAIN_FULL)
@@ -9,8 +11,117 @@ y_train = pd.read_pickle(consts.Y_TRAIN_FULL)
 x_test = pd.read_pickle(consts.X_TEST_FULL)
 y_test = pd.read_pickle(consts.Y_TEST_FULL)
 
-y_train = pd.DataFrame(y_train)
+y_train = pd.DataFrame({'SalePrice': y_train}, index=x_train.index)
+train = pd.merge(x_train, y_train, on=x_train.index)
+
+y_test = pd.DataFrame({'SalePrice': y_test}, index=x_test.index)
+test = pd.merge(x_test, y_test, on=x_test.index)
+
+(mu, sigma) = norm.fit(train['SalePrice'])
+print('mu = {:.2f} and sigma = {:.2f}\n'.format(mu, sigma))
+
+train["SalePrice"] = np.log1p(train["SalePrice"])
+(mu, sigma) = norm.fit(train['SalePrice'])
+print('mu = {:.2f} and sigma = {:.2f}\n'.format(mu, sigma))
+
+ntrain = train.shape[0]
+ntest = test.shape[0]
+y_train = train.SalePrice.values
+all_data = pd.concat((train, test)).reset_index(drop=True)
+all_data.drop(['SalePrice'], axis=1, inplace=True)
+print("all_data size is : {}".format(all_data.shape))
+
+all_data_na = (all_data.isnull().sum() / len(all_data)) * 100
+all_data_na = all_data_na.drop(all_data_na[all_data_na == 0].index).sort_values(ascending=False)[:30]
+missing_data = pd.DataFrame({'Missing Ratio': all_data_na})
+print(missing_data.head(20))
+
+all_data["PoolQC"] = all_data["PoolQC"].fillna("None")
+all_data["MiscFeature"] = all_data["MiscFeature"].fillna("None")
+all_data["Alley"] = all_data["Alley"].fillna("None")
+all_data["Fence"] = all_data["Fence"].fillna("None")
+all_data["FireplaceQu"] = all_data["FireplaceQu"].fillna("None")
+# Group by neighborhood and fill in missing value by the median LotFrontage of all the neighborhood
+all_data["LotFrontage"] = all_data.groupby("Neighborhood")["LotFrontage"].transform(
+    lambda x: x.fillna(x.median()))
+
+for col in ('GarageType', 'GarageFinish', 'GarageQual', 'GarageCond'):
+    all_data[col] = all_data[col].fillna('None')
+for col in ('GarageYrBlt', 'GarageArea', 'GarageCars'):
+    all_data[col] = all_data[col].fillna(0)
+for col in ('BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', 'BsmtFullBath', 'BsmtHalfBath'):
+    all_data[col] = all_data[col].fillna(0)
+for col in ('BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2'):
+    all_data[col] = all_data[col].fillna('None')
+all_data["MasVnrType"] = all_data["MasVnrType"].fillna("None")
+all_data["MasVnrArea"] = all_data["MasVnrArea"].fillna(0)
+all_data['MSZoning'] = all_data['MSZoning'].fillna(all_data['MSZoning'].mode()[0])
+all_data = all_data.drop(['Utilities'], axis=1)
+all_data["Functional"] = all_data["Functional"].fillna("Typ")
+all_data['Electrical'] = all_data['Electrical'].fillna(all_data['Electrical'].mode()[0])
+all_data['KitchenQual'] = all_data['KitchenQual'].fillna(all_data['KitchenQual'].mode()[0])
+all_data['Exterior1st'] = all_data['Exterior1st'].fillna(all_data['Exterior1st'].mode()[0])
+all_data['Exterior2nd'] = all_data['Exterior2nd'].fillna(all_data['Exterior2nd'].mode()[0])
+all_data['SaleType'] = all_data['SaleType'].fillna(all_data['SaleType'].mode()[0])
+all_data['MSSubClass'] = all_data['MSSubClass'].fillna("None")
+
+# Check remaining missing values if any
+all_data_na = (all_data.isnull().sum() / len(all_data)) * 100
+all_data_na = all_data_na.drop(all_data_na[all_data_na == 0].index).sort_values(ascending=False)
+missing_data = pd.DataFrame({'Missing Ratio': all_data_na})
+print('\nПроверка: ', missing_data.empty)
+
+# MSSubClass=The building class
+all_data['MSSubClass'] = all_data['MSSubClass'].apply(str)
+# Changing OverallCond into a categorical variable
+all_data['OverallCond'] = all_data['OverallCond'].astype(str)
+# Year and month sold are transformed into categorical features.
+all_data['YrSold'] = all_data['YrSold'].astype(str)
+all_data['MoSold'] = all_data['MoSold'].astype(str)
+
+cols = ('FireplaceQu', 'BsmtQual', 'BsmtCond', 'GarageQual', 'GarageCond',
+        'ExterQual', 'ExterCond', 'HeatingQC', 'PoolQC', 'KitchenQual', 'BsmtFinType1',
+        'BsmtFinType2', 'Functional', 'Fence', 'BsmtExposure', 'GarageFinish', 'LandSlope',
+        'LotShape', 'PavedDrive', 'Street', 'Alley', 'CentralAir', 'MSSubClass', 'OverallCond',
+        'YrSold', 'MoSold')
+# process columns, apply LabelEncoder to categorical features
+for c in cols:
+    lbl = LabelEncoder()
+    lbl.fit(list(all_data[c].values))
+    all_data[c] = lbl.transform(list(all_data[c].values))
+
+# shape
+print('Shape all_data: {}'.format(all_data.shape))
+
+# Adding total sqfootage feature
+all_data['TotalSF'] = all_data['TotalBsmtSF'] + all_data['1stFlrSF'] + all_data['2ndFlrSF']
+
+numeric_feats = all_data.dtypes[all_data.dtypes != "object"].index
+# Check the skew of all numerical features
+skewed_feats = all_data[numeric_feats].apply(lambda x: skew(x.dropna())).sort_values(ascending=False)
+print("\nSkew in numerical features: \n")
+skewness = pd.DataFrame({'Skew': skewed_feats})
+print(skewness.head(10))
 
 
 
-#
+skewness = skewness[abs(skewness) > 0.75]
+print("There are {} skewed numerical features to Box Cox transform".format(skewness.shape[0]))
+from scipy.special import boxcox1p
+skewed_features = skewness.index
+lam = 0.15
+for feat in skewed_features:
+    # all_data[feat] += 1
+    all_data[feat] = boxcox1p(all_data[feat], lam)
+# all_data[skewed_features] = np.log1p(all_data[skewed_features])
+
+
+# Getting dummy categorical features
+all_data = pd.get_dummies(all_data)
+print(all_data.shape)
+
+# Getting the new train and test sets.
+train = all_data[:ntrain]
+test = all_data[ntrain:]
+
+

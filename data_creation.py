f75d5c0fdb7a445b6edf8895a302b2feceb854fe
Artem Mirvoda <mirvodartem@gmail.com>
2023-03-07 00:01:15 +0500

    Update logging at data_creation

--- data_creation.py
+++ data_creation.py
@@ -5,15 +5,15 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 
 import consts
-from utils import time_now
+from utils import print_log
 
-print(f'{time_now()} Loading data')
+print_log('Loading data')
 data = pd.read_csv(consts.DATA_PATH, delimiter=',', index_col='Id')
 x = data.iloc[:, 0:-1]
 y = data.SalePrice.values
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=consts.DATA_TEST_SIZE, random_state=consts.RANDOM_STATE)
 
-print(f'{time_now()} Saving data')
+print_log('Saving data')
 Path(consts.X_TRAIN).mkdir(parents=True, exist_ok=True)
 with open(consts.X_TRAIN_FULL, 'wb') as f:
   pickle.dump(X_train, f)

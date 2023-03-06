1444e70b6bcd5f9aeb0fa93a4ff8c711e341fcb3
Artem Mirvoda <mirvodartem@gmail.com>
2023-03-07 00:24:22 +0500

    Formatted model preparation

--- model_preparation.py
+++ model_preparation.py
@@ -17,6 +17,7 @@ from sklearn.model_selection import KFold, cross_val_score, train_test_split
 from sklearn.metrics import mean_squared_error
 
 import consts
+from utils import print_log
 
 N_FOLDS = 5
 
@@ -27,17 +28,14 @@ class AveragingModels(BaseEstimator, RegressorMixin, TransformerMixin):
     def __init__(self, models):
         self.models = models
         
-    # we define clones of the original models to fit the data in
     def fit(self, X, y):
         self.models_ = [clone(x) for x in self.models]
         
-        # Train cloned base models
         for model in self.models_:
             model.fit(X, y)
 
         return self
     
-    #Now we do the predictions for cloned models and average them
     def predict(self, X):
         predictions = np.column_stack([
             model.predict(X) for model in self.models_
@@ -82,12 +80,15 @@ class StackingAveragedModels(BaseEstimator, RegressorMixin, TransformerMixin):
 def rmsle_cv(model, model_name:str =None):
     kf = KFold(N_FOLDS, shuffle=True, random_state=42).get_n_splits(x_train.values)
     rmse = np.sqrt(-cross_val_score(model, x_train.values, y_train, scoring="neg_mean_squared_error", cv = kf))
-    print(f"{model_name if model_name else model} score: {rmse.mean():.4f} ({rmse.std():.4f})\n")
+    print(f"{model_name if model_name else model} \nRMSE mean: {rmse.mean():.4f} \nRMSE std: {rmse.std():.4f}")
     return(rmse)
+
+print_log('Data read')
 
 X: pd.DataFrame = pd.read_pickle(consts.X_TRAIN_FULL)
 Y: pd.DataFrame = pd.read_pickle(consts.Y_TRAIN_FULL)
 
+print_log('Models init')
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=consts.DATA_TEST_SIZE, random_state=consts.RANDOM_STATE)
 lasso = make_pipeline(RobustScaler(), Lasso(alpha =0.0005, random_state=1))
 ENet = make_pipeline(RobustScaler(), ElasticNet(alpha=0.0005, l1_ratio=.9, random_state=3))
@@ -96,18 +97,18 @@ GBoost = GradientBoostingRegressor(n_estimators=3000, learning_rate=0.05,
                                 max_depth=4, max_features='sqrt',
                                 min_samples_leaf=15, min_samples_split=10, 
                                 loss='huber', random_state =5)
-model_xgb = xgb.XGBRegressor(colsample_bytree=0.4603, gamma=0.0468, 
-                            learning_rate=0.05, max_depth=3, 
-                            min_child_weight=1.7817, n_estimators=2200,
-                            reg_alpha=0.4640, reg_lambda=0.8571,
-                            subsample=0.5213, silent=1,
-                            random_state =7, nthread = -1)
-model_lgb = lgb.LGBMRegressor(objective='regression',num_leaves=5,
-                            learning_rate=0.05, n_estimators=720,
+model_xgb = xgb.XGBRegressor(gamma = 0.0468, 
+                            learning_rate = 0.05, max_depth = 3, 
+                            min_child_weight=1.7817, n_estimators = 2200,
+                            reg_alpha = 0.4640, reg_lambda=0.8571,
+                            subsample = 0.5213, silent=1,
+                            random_state = 7, nthread = -1)
+model_lgb = lgb.LGBMRegressor(objective = 'regression', num_leaves = 5,
+                            learning_rate = 0.05, n_estimators = 720,
                             max_bin = 55, bagging_fraction = 0.8,
                             bagging_freq = 5, feature_fraction = 0.2319,
-                            feature_fraction_seed=9, bagging_seed=9,
-                            min_data_in_leaf =6, min_sum_hessian_in_leaf = 11)
+                            feature_fraction_seed=9, bagging_seed = 9,
+                            min_data_in_leaf = 6, min_sum_hessian_in_leaf = 11)
 
 
 averaged_models = AveragingModels(models = (ENet, GBoost, KRR, lasso))
@@ -115,10 +116,15 @@ stacked_averaged_models = StackingAveragedModels(base_models = (ENet, GBoost, KR
                                                 meta_model = lasso)
 
 models = [lasso, ENet, KRR, GBoost, model_xgb, model_lgb, averaged_models, stacked_averaged_models]
+
+print_log('Models testing:')
 results = list(zip(map(lambda model: rmsle_cv(model), models), models))
 model = reduce(lambda res1, res2: res1 if np.average(res1[0]) > np.average(res2[0]) else res2, results)[1]
+
+print_log('Fit best model')
 model.fit(X, Y)
 
+print_log('Save best model')
 Path(consts.MODEL).mkdir(parents=True, exist_ok=True)
 with open(consts.MODEL_FULL, 'wb') as f:
   pickle.dump(model, f)

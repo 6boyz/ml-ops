bc62d564a87f655decdfc5968065e173a1ec64ec
Artem Mirvoda <mirvodartem@gmail.com>
2023-03-06 22:34:59 +0500

    Fixed uncorrect order at pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -4,7 +4,7 @@ python3 -m venv .venv
 python --version
 pip install -r requirements.txt
 python data_creation.py
-python model_preparation.py
 python model_preprocessing.py
+python model_preparation.py
 python model_testing.py
 read

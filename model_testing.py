1eb95d0c796862d18d594d1ad1bf14c251c1d5c5
Artem Mirvoda <mirvodartem@gmail.com>
2023-03-06 22:34:32 +0500

    Fixed consts import at model testing

--- model_testing.py
+++ model_testing.py
@@ -2,9 +2,10 @@ import consts
 import pickle
 from sklearn.metrics import r2_score
 
-model = pickle.load(open(MODEL_FULL, "rb"))
-x_test = pickle.load(open(X_TEST_FULL, "rb"))
-y_test = pickle.load(open(Y_TEST_FULL, "rb"))
+
+model = pickle.load(open(consts.MODEL_FULL, "rb"))
+x_test = pickle.load(open(consts.X_TEST_FULL, "rb"))
+y_test = pickle.load(open(consts.Y_TEST_FULL, "rb"))
 
 y_pred = model.predict(x_test)
 score = r2_score(y_test, y_pred)

a56bb1ad75c5d1bf38b5849aff2528d8b0eb09b7
Artem Mirvoda <mirvodartem@gmail.com>
2023-03-24 00:45:18 +0500

    Ubuntu 22 script by default

--- pipeline.sh
+++ pipeline.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 DATE_BIN=$(command -v date)
 DATE=`${DATE_BIN} +@%H:%M:%S`
 
@@ -18,30 +20,29 @@ print_log Install pip...
 apt -y install python3-pip
 
 print_log Install virtualenv for Python...
-python3 -m pip install --user virtualenv
+apt -y install python3.10-venv
 
 print_log Create Python venv with name .venv...
 python3 -m venv .venv
 
 print_log Activate venv...
-source .venv/Scripts/activate
+source .venv/bin/activate
 
-print_log Current version: `python --version`
+print_log Current version: `python3 --version`
 
 print_log Install requirements...
 pip install -r requirements.txt
 
 print_log Data creation script run...
-python data_creation.py
+python3 data_creation.py
 
 print_log Model preprocessing script run...
-python model_preprocessing.py
+python3 model_preprocessing.py
 
 print_log Model preparation script run...
-python model_preparation.py
+python3 model_preparation.py
 
 print_log Model testing script run...
-python model_testing.py
+python3 model_testing.py
 
 print_log Done
-read

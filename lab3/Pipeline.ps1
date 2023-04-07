b9870c2e098463ec2d4a0a08f55d543e2b5a61b2
Artem Mirvoda <mirvodartem@gmail.com>
2023-04-07 21:45:36 +0500

    Added input for pipelines

--- lab3/Pipeline.ps1
+++ lab3/Pipeline.ps1
@@ -1,5 +1,10 @@
-$USERNAME="username"
-$PASSWORD="password"
+$USERNAME = Read-Host 'What is your Docker Hub account username?'
+$PASSWORD = Read-Host 'What is your Docker Hub account password?' -AsSecureString
+
+$PASSWORD = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($PASSWORD))
+
+#$USERNAME="username"
+#$PASSWORD="password"
 
 $APP_NAME="answers-to-questions"
 $TAG=git rev-parse --short HEAD
@@ -8,4 +13,5 @@ $APP_KEY = "$($USERNAME)/$($APP_NAME):$($TAG)"
 docker login -u $USERNAME -p $PASSWORD 
 docker compose build
 docker tag $APP_NAME $APP_KEY
+docker image rm $APP_NAME
 docker push $APP_KEY
--- lab3/pipeline.sh
+++ lab3/pipeline.sh
@@ -1,5 +1,14 @@
-USERNAME="username"
-PASSWORD="password"
+#!/bin/sh
+
+echo "What is your Docker Hub account username?"
+read -r USERNAME
+echo "What is your Docker Hub account password?"
+stty -echo
+read -r PASSWORD
+stty echo
+
+#USERNAME="username"
+#PASSWORD="password"
 
 APP_NAME="answers-to-questions"
 TAG=$(git rev-parse --short HEAD)
@@ -8,4 +17,5 @@ APP_KEY="$($USERNAME)/$($APP_NAME):$($TAG)"
 docker login -u $USERNAME -p $PASSWORD 
 docker compose build
 docker tag $APP_NAME $APP_KEY
+docker image rm $APP_NAME
 docker push $APP_KEY

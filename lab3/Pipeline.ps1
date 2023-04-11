691b186793e7362b3aa4be8ec99ec80e357af8a0
Artem Mirvoda <mirvodartem@gmail.com>
2023-04-11 21:53:03 +0500

    Fixed scripts setting tag from current repo, added 200iq moves, refactor

--- lab3/Pipeline.ps1
+++ lab3/Pipeline.ps1
@@ -1,27 +1,30 @@
+$ErrorActionPreference = "Stop" # Exit after first error code
+
 $USERNAME = Read-Host 'What is your Docker Hub account username?'
 $PASSWORD = Read-Host 'What is your Docker Hub account password?' -AsSecureString
-
 $PASSWORD = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($PASSWORD))
+$APP_NAME = "answers-to-questions"
 
 #$USERNAME="username"
 #$PASSWORD="password"
 
-$APP_NAME = "answers-to-questions"
+docker login -u $USERNAME -p $PASSWORD 
 
+if (Test-Path "app") {
+  Remove-Item "app" -Recurse -Force -Confirm:$false
+}
 git clone https://github.com/6boyz/project-practicum.git app
 cd app
-
 $TAG = git rev-parse --short HEAD
 $APP_KEY = "$($USERNAME)/$($APP_NAME):$($TAG)"
-
 cd ..
 
-docker login -u $USERNAME -p $PASSWORD 
 docker compose build
 docker tag $APP_NAME $APP_KEY
 docker image rm $APP_NAME
 docker push $APP_KEY
-docker run -p 8501:8501 $APP_KEY
+docker run -d -p 8501:8501 $APP_KEY
 
-Write-Output "Application available on DockerHub: https://hub.docker.com/repository/docker/$($USERNAME)/$($APP_NAME)/"
-Write-Output 'Application started at http://localhost:8501...'
+Write-Output "Application: $APP_KEY"
+Write-Output "On DockerHub: https://hub.docker.com/repository/docker/$($USERNAME)/$($APP_NAME)/"
+Write-Output "Started at http://localhost:8501"
--- lab3/pipeline.sh
+++ lab3/pipeline.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -Eeuo pipefail # Exit after first error code
 
 echo "What is your Docker Hub account username?"
 read -r USERNAME
@@ -9,17 +10,24 @@ stty echo
 
 #USERNAME="username"
 #PASSWORD="password"
-
 APP_NAME="answers-to-questions"
-TAG=$(git rev-parse --short HEAD)
-APP_KEY="$USERNAME/$APP_NAME:$TAG"
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+APP_PATH="$SCRIPT_DIR/app/"
 
 docker login -u $USERNAME -p $PASSWORD 
+
+if [ -d "$APP_PATH" ]; then rm -Rf $APP_PATH; fi
+git clone https://github.com/6boyz/project-practicum.git app
+cd $SCRIPT_DIR
+TAG=$(git rev-parse --short HEAD)
+APP_KEY="$USERNAME/$APP_NAME:$TAG"
+cd -
+
 docker compose build
 docker tag $APP_NAME $APP_KEY
 docker image rm $APP_NAME
 docker push $APP_KEY
-docker run -p 8501:8501 $APP_KEY
+docker run -d -p 8501:8501 $APP_KEY
 
 echo "Application available on DockerHub: https://hub.docker.com/repository/docker/$USERNAME/$APP_NAME/"
 echo "Application started at http://localhost:8501..."
